a0aed0e3255c696e7487685eada28f10da18028c
jiaruchun <jiaruchun83@gmail.com>
2025-06-12 23:57:16 +0800

    苍穹外卖完工

--- cangqiong-parent/cangqiong-service/src/test/java/jiaruchun/cangqiongservice/CangqiongServiceApplicationTests.java
+++ cangqiong-parent/cangqiong-service/src/test/java/jiaruchun/cangqiongservice/CangqiongServiceApplicationTests.java
@@ -2,6 +2,7 @@ package jiaruchun.cangqiongservice;
 
 import jiaruchun.cangqiongservice.mapper.OrderDetailMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,5 +14,8 @@ class CangqiongServiceApplicationTests {
     private RedisTemplate<String,Object> redisTemplate;
     @Autowired
     private OrderDetailMapper orderDetailMapper;
-
+    @Test
+    public void redisTest(){
+        
+    }
 }
